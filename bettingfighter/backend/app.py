48aed642a2392c9120ee0c40f2efb2c322c9acb9
EmmS21 <44911198+EmmS21@users.noreply.github.com>
2023-02-01 14:33:54 -0600

    model working

--- bettingfighter/backend/app.py
+++ bettingfighter/backend/app.py
@@ -1,45 +1,53 @@
-from flask import Flask, request, jsonify
+from flask import Flask, request
 import pickle
 import pandas as pd
-import catboost
 app = Flask(__name__)
 
 @app.route('/predict', methods=['GET','POST'])
 
 def home():
     df = pd.DataFrame(request.json["challenge"])
-    cat_boost_model = pickle.load(open("C:/Users/jasmi/OneDrive/Documents/GitHub/boxing/SpringboardCapstoneBoxingPredictionWebApp/ml model/app/boxing/catmodelsummary.pkl","rb"))
-    pred = df[["Win", "Loss", "KO", "KnockedOut", "last6"]]
-
-    df_win = pd.DataFrame(df['Win'].values.tolist(), index=df.index)
-    df_loss = pd.DataFrame(df['Loss'].values.tolist(), index=df.index)
-    df_knockedout = pd.DataFrame(df['KnockedOut'].values.tolist(), index=df.index)
-    df_ko_ratio = pd.DataFrame(df['KO'].values.tolist(), index=df.index)
-    pred[['winKO', 'winOther']] = df_win
-    pred[['lossKO', 'lossOther']] = df_loss
-    pred['Knockedoutratio'] = df_knockedout
-    pred['KOratio'] = df_ko_ratio
-    pred['opp_last6'] = pred['last6']
-    pred.drop(["Win","Loss", "KO", "KnockedOut","last6"], axis=1, inplace=True)
-    pred = pred.unstack().to_frame().T
-    pred.columns = pred.columns.map('{0[0]}_{0[1]}'.format)
-    pred.rename(columns={"WinKO_0": "WinKO", "lossOther_0": "lossOther", "winOther_0": "winOther", "KOratio_0": "KOratio", "Knockedoutratio_0": "Knockedoutratio",
-                                "opp_last6_1": "opp_last6", "KOratio_1": "oppKOratio", "winKO_1": "opp_WinKO", "lossOther_1": "opp_loss",
-                                "lossOther_1": "opp_lossOther", "winOther_1":"opp_win", "lossKO_1": "opp_lossKO"})
-    print(pred.columns)
-    pred[['WinKO', "opp_winKO", "winOther", "opp_win", "lossKO", "opp_lossKO", "lossOther", "opp_lossOther", 
-          "Knockedoutratio", "opp_knockedoutratio", "KOratio", "oppKOratio", "last6", "opp_last6"]] = pred[["winKO_0", "winKO_1", "winOther_0", "winOther_1", "lossKO_0",
-                                                                                                            "lossKO_1", "lossOther_0", "lossOther_1", "Knockedoutratio_0",
-                                                                                                            "Knockedoutratio_1", "KOratio_0", "KOratio_1", "opp_last6_0",
-                                                                                                            "opp_last6_1"]]
-    pred.drop(["winKO_0", "winKO_1", "winOther_0", "winOther_1", "lossKO_0", "lossKO_1", "lossOther_0", "lossOther_1", "Knockedoutratio_0",
-               "Knockedoutratio_1", "KOratio_0", "KOratio_1", "opp_last6_0", "opp_last6_1"], axis=1, inplace=True)
-    cat_boost_model.predict(pred)
-    print(cat_boost_model)
-
-    # print(complete_df)
-
-    return 'Works'
+    random_forest = pickle.load(open("C:/Users/jasmi/OneDrive/Documents/GitHub/boxing/SpringboardCapstoneBoxingPredictionWebApp/ml model/randomforestModelComplete2.pkl","rb"))
+    pred = df[["Win", "Loss", "KO", "KnockedOut", "last6", "Draw", "weight", "fight_count",
+            "avgpts", "avgptsagainst", "age", "height", "Jab",
+            "Power", "Total", "Avg", "opp_last6"]]
+    pred_jabaccuracy = pd.DataFrame(pred['Jab'].values.tolist(), index=pred.index)
+    pred_power = pd.DataFrame(pred['Power'].values.tolist(), index=pred.index)
+    pred_total = pd.DataFrame(pred['Total'].values.tolist(), index=pred.index)
+    pred_avg = pd.DataFrame(pred['Avg'].values.tolist(), index=pred.index)
+    pred_avgjabs = pd.DataFrame(pred_avg['Jabs'].values.tolist(), index=pred_avg.index)
+    pred_avgpower = pd.DataFrame(pred_avg['Power'].values.tolist(), index=pred_avg.index)
+    pred_avgtotal = pd.DataFrame(pred_avg['Total'].values.tolist(), index=pred_avg.index)
+    pred['Jab.accuracy'] = pred_jabaccuracy
+    pred['Total.punch.accuracy'] = pred_total
+    pred['Avg.Jabs.landed'] = pred_avgjabs
+    pred['Avg.Power.punches.landed'] = pred_avgpower
+    pred['Avg.Total.punches.landed'] = pred_avgtotal
+    pred["Power.punch.accuracy"] = pred_power
+    pred_win = pd.DataFrame(pred['Win'].values.tolist(), index=pred.index)
+    pred_loss = pd.DataFrame(pred['Loss'].values.tolist(), index=pred.index)
+    pred_knockedout = pd.DataFrame(pred['KnockedOut'].values.tolist(), index=pred.index)
+    pred_ko_ratio = pd.DataFrame(pred['KO'].values.tolist(), index=pred.index)
+    pred[['Win.KO', 'Win.Other']] = pred_win
+    pred[['Loss.KO', 'Loss.Other']] = pred_loss
+    pred['KnockedOut.ratio'] = pred_knockedout
+    pred['KO.ratio'] = pred_ko_ratio
+    pred.drop(["Win","Loss", "KO", "KnockedOut", "Jab", "Power", "Total", "Avg"], axis=1, inplace=True)
+    pred[["opp_last6","Draw", "weight", "Loss.KO", "Loss.Other", "Win.KO", "Win.Other","fight_count",
+             "avgpts","avgptsagainst", "KO.ratio", "KnockedOut.ratio", "age", "height",
+            "Jab.accuracy", "Power.punch.accuracy", "Total.punch.accuracy", "Avg.Jabs.landed","Avg.Power.punches.landed",
+            "Avg.Total.punches.landed", "last6"]] =  pred[["opp_last6","Draw", "weight", "Loss.KO", "Loss.Other", "Win.KO", "Win.Other","fight_count",
+                                                           "avgpts","avgptsagainst", "KO.ratio", "KnockedOut.ratio", "age", "height",
+                                                           "Jab.accuracy", "Power.punch.accuracy", "Total.punch.accuracy", "Avg.Jabs.landed","Avg.Power.punches.landed",
+                                                           "Avg.Total.punches.landed", "last6"]]
+    pred["Power.punch.accuracy"] = pd.json_normalize(pred["Power.punch.accuracy"])
+    pred["Total.punch.accuracy"] = pd.json_normalize(pred["Total.punch.accuracy"])
+    pred["Avg.Jabs.landed"] = pd.json_normalize(pred["Avg.Jabs.landed"])
+    pred["Avg.Power.punches.landed"] = pd.json_normalize(pred["Avg.Power.punches.landed"])
+    pred["Avg.Total.punches.landed"] = pd.json_normalize(pred["Avg.Total.punches.landed"])
+    result = random_forest.predict_proba(pred)
+    result = pd.DataFrame(result)
+    return pd.DataFrame.to_json(result)
 
 app.run(port=5000)
 
